8d9afc7a59151d6f3fcb409356ace004d95c23fd
d4n3436 <dan3436@hotmail.com>
2023-04-07 21:19:33 -0500

    Make ButtonContext a struct and public

--- src/Pagination/ButtonContext.cs
+++ src/Pagination/ButtonContext.cs
@@ -1,10 +1,17 @@
 namespace Fergun.Interactive.Pagination;
 
 /// <inheritdoc cref="IButtonContext"/>
-internal sealed class ButtonContext : IButtonContext
+public readonly struct ButtonContext : IButtonContext
 {
     private readonly bool _disableAll;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ButtonContext"/> structure.
+    /// </summary>
+    /// <param name="buttonIndex">The index of the button within the components.</param>
+    /// <param name="currentPageIndex">The index of the page that will be displayed.</param>
+    /// <param name="maxPageIndex">The maximum page index.</param>
+    /// <param name="disableAll">Whether to disable all the buttons.</param>
     public ButtonContext(int buttonIndex, int currentPageIndex, int maxPageIndex, bool disableAll)
     {
         ButtonIndex = buttonIndex;
