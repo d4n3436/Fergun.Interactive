dee8e8a16ab82ae2f328b123ebd05335bff612a0
zobweyt <zobweyt@gmail.com>
2023-07-15 13:44:16 +0300

    Add `UseInteractiveService` extension method for `IHostBuilder` to simplify hosting

---
+++ src/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Fergun.Interactive.Extensions;
+
+/// <summary>
+/// Provides extension methods for <see cref="IHostBuilder"/>.
+/// </summary>
+public static class HostBuilderExtensions
+{
+    /// <summary>
+    /// Adds <see cref="InteractiveService"/> and optionally configures a <see cref="InteractiveConfig"/> along with the required services.
+    /// </summary>
+    /// <param name="builder">The host builder to configure.</param>
+    /// <param name="config">The delegate for the <see cref="InteractiveConfig" /> that will be used to configure the host.</param>
+    /// <returns>The generic host builder.</returns>
+    public static IHostBuilder UseInteractiveService(this IHostBuilder builder, Action<HostBuilderContext, InteractiveConfig>? config = null)
+    {
+        return builder.ConfigureServices((context, collection) =>
+        {
+            if (config != null)
+                collection.AddSingleton(config);
+
+            collection.AddSingleton<InteractiveService>();
+        });
+    }
+}
